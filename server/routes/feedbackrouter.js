ba314e79ec7564aa9d1c33644b090a71cf72a484
Bobby Khounphinith <khoupe07@gmail.com>
2019-05-10 16:42:17 -0500

    Created POST route to add info to database

--- server/routes/feedbackrouter.js
+++ server/routes/feedbackrouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+
+//GET route to get info from database
 router.get('/' , (req, res) => {
     let queryText = `SELECT * FROM "feedback";`;
     pool.query(queryText)
@@ -12,5 +14,21 @@ router.get('/' , (req, res) => {
         res.sendStatus(500)
     })
 })
+
+router.post('/', (req, res) => {
+    console.log('In POST route', req.body);
+    const addFeedback = req.body;
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                        VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [addFeedback.feeling, addFeedback.understanding, 
+                            addFeedback.support, addFeedback.comments])
+    .then((response) => {
+        console.log(response);
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log('Error in POST route', error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
